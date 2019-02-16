1994d6ad905e12350966815dede6f714725cc585
tonikelope <tonikelope@gmail.com>
2019-02-16 12:27:35 +0100

    1.

--- tuneladora.py
+++ tuneladora.py
@@ -8,7 +8,7 @@ import re
 from pyfiglet import figlet_format
 from termcolor import cprint, colored
 
-VERSION = "1.6"
+VERSION = "1.7"
 
 def parse_ports(args_ports):
 	ports = []
@@ -19,15 +19,11 @@ def parse_ports(args_ports):
 
 		ports_split2 = [i.strip() for i in port1.split('#')]
 
-		if len(ports_split2) == 3:
-			ports_append = {'laddress': ports_split2[0], 'raddress': ports_split2[1], 'ports': []}
-			ports_info = ports_split2[2]
-		elif len(ports_split2) == 2:
-			ports_append = {'laddress': 'localhost', 'raddress': ports_split2[0], 'ports': []}
-			ports_info = ports_split2[1]
-		elif len(ports_split2) == 1:
-			ports_append = {'laddress': 'localhost', 'raddress': 'localhost', 'ports': []}
-			ports_info = ports_split2[0]
+		l = len(ports_split2)
+
+		ports_append = {'laddress': ports_split2[l-3] if l==3 else 'localhost', 'raddress': ports_split2[l-2] if l >= 2 else 'localhost', 'ports': []}
+		
+		ports_info = ports_split2[l-1]
 
 		ports_split3 = [i.strip() for i in ports_info.split(',')]
 
@@ -57,13 +53,15 @@ cprint(figlet_format("Tuneladora SSH"), "green", attrs=["bold"])
 
 cprint("Tuneladora SSH "+VERSION+"\nA SSH port redirector for lazy people (made with love by tonikelope).", attrs=["bold"])
 
+cprint("https://github.com/tonikelope/tuneladora\n")
+
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=colored("Some examples:\n\n#1")+colored(" tuneladora 'localhost#localhost#8080' user@192.168.1.5", "green")+colored(" Redirects local port 8080 of (local) localhost to remote port 8080 of (remote) localhost on remote machine 192.168.1.5")+colored("\n\n#2")+colored(" tuneladora 8080 user@192.168.1.5", "green")+colored(" Same as #1 (default local/remote address is 'localhost')")+colored("\n\n#3")+colored(" tuneladora '8080:8081' user@192.168.1.5", "green")+colored(" Redirects local ports 8080 to 8081 of (local) localhost to remote ports 8080 to 8081 of (remote) localhost on remote machine 192.168.1.5")+colored("\n\n#4")+colored(" tuneladora '9000->10000' user@192.168.1.5", "green")+colored(" Redirects local port 9000 of (local) localhost to remote port 10000 of (remote) localhost on remote machine 192.168.1.5")+colored("\n\n#5")+colored(" tuneladora '192.168.100.3#localhost#8080,9090+192.168.1.5#9000->9100+10000:10005' user@192.168.1.5", "green")+colored(" Redirects local ports 8080 and 9090 of (local) 192.168.100.3 to remote ports 8080 and 9090 of (remote) localhost AND redirects local port 9000 of (local) localhost to remote port 9100 of (remote) 192.168.1.5 AND redirects local ports 10000 to 10005 of (local) localhost to remote ports 10000 to 10005 of (remote) localhost ON remote machine 192.168.1.5"))
 
 parser.add_argument("ports", help="[[local_address#]remote_address#]<lport[->rport]|port_init:port_end[,lport[->rport]|port_init:port_end[,...]]>[+[[local_address#]remote_address#]<lport[->rport]|port_init:port_end[,lport[->rport]|port_init:port_end[,...]]>[+...]]")
 
-parser.add_argument("destination", help="Destination host.")
+parser.add_argument("destination", help="[user@]host")
 
-parser.add_argument('proxy', nargs='?', default=None, help="ssh -o ProxyCommand='THIS STUFF' (OPTIONAL)")
+parser.add_argument('proxy', nargs='?', default=None, help="ssh ProxyCommand option")
 
 args = parser.parse_args()
 
